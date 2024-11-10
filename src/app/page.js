82f5ef3669a2290f685a4bee82c2271bd8ba3f9d
alisoneoz <alisonestephanyoz@gmail.com>
2024-11-10 02:16:38 -0400

    update imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
 'use client';
-import Introduction from '@/components/sections/Introduction'
-import ModelsShowcase from '@/components/sections/ModelsShowcase'
+
 import Container from '@/components/ui/container'
 import InteractiveCubeSection from '@/components/sections/InteractiveCubeSection'
 import SecondSection from '@/components/sections/SecondSection'
 import Hero from '@/components/sections/Hero';
-import JourneySection from '@/components/sections/JourneySection';
-import CreditsSection from '@/components/sections/CreditsSection';
-import GalleryFinale from '@/components/sections/GalleryFinale';
 
 export default function Home() {
   return (
